efe5c103d8ca1aea4d4cc5e61172b50a5021de56
Quocvi1701 <vi.ha@vietguys.biz>
2024-09-30 10:55:24 +0700

    refactor: change color

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
 import '@/assets/css/index.css';
+import '@/assets/sass/custom.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react({
-            babel: {
-                plugins: [
-                    ['@babel/plugin-proposal-decorators', { legacy: true }],
-                    ['@babel/plugin-proposal-class-properties', { loose: true }]
-                ]
-            }
-        }),
-        tsconfigPaths()
-    ]
+  base: './',
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['@babel/plugin-proposal-decorators', { legacy: true }],
+          ['@babel/plugin-proposal-class-properties', { loose: true }],
+        ],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  optimizeDeps: { exclude: ['fsevents'] },
+  build: {
+    rollupOptions: {
+      external: ['fs/promises'],
+      output: {
+        experimentalMinChunkSize: 3500,
+      },
+    },
+  },
 });
